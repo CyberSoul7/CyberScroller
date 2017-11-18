13bb4d9abc6bffbc98fbccde6c5961192f0bdade
William Keller <komputerbuilder@gmail.com>
2017-11-18 14:54:26 -0600

    Added res folder.
    Contains sounds folder.
    Contains textures folder.

--- CyberScroller/src/net/griscraft/cyberscroller/Game.java
+++ CyberScroller/src/net/griscraft/cyberscroller/Game.java
@@ -3,9 +3,7 @@ package net.griscraft.cyberscroller;
 public class Game {
 	
 	public Game() {
-		System.out.println("The program ran!");
-		System.out.println("-----------------");
-		System.out.println("Test 1");
+		
 	}
 	
 	public static void main(String[] args) {
