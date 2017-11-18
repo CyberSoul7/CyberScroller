e273ee4a4c2bbdbe15fd769e1f82b7f9c932d5e1
William Keller <komputerbuilder@gmail.com>
2017-11-18 14:00:42 -0600

    Initial Commit

---
+++ CyberScroller/src/net/griscraft/cyberscroller/Game.java
@@ -0,0 +1,13 @@
+package net.griscraft.cyberscroller;
+
+public class Game {
+	
+	public Game() {
+		System.out.println("The program ran.");
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
+	
+}
