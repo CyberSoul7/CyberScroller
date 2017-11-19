dca30e409205f199c1d7f10788cac51a8d9e37d8
William Keller <komputerbuilder@gmail.com>
2017-11-18 19:38:31 -0600

    Created Game Loop

--- CyberScroller/src/net/griscraft/cyberscroller/Game.java
+++ CyberScroller/src/net/griscraft/cyberscroller/Game.java
@@ -8,11 +8,65 @@ public class Game extends Canvas implements Runnable {
 	
 	public static final int width = 1000, height = 563;
 	
+	private boolean isRunning = false;
+	private Thread thread;
+	
+	private int fps;
+	
 	public Game() {
 		new Window(width, height, "CyberScroller", this);
+		
+		start();
+	}
+	
+	private void start() {
+		isRunning = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	private void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void run() {
+		requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (isRunning) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				delta--;
+			}
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				fps = frames;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	private void tick() {
+		
+	}
+	
+	private void render() {
 		
 	}
 	
