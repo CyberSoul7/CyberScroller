cbf4050d30bf24dbff77945747ce81f5c6fd7270
William Keller <komputerbuilder@gmail.com>
2017-11-19 07:30:48 -0600

    Implemented Movement

--- CyberScroller/src/net/griscraft/cyberscroller/objects/Player.java
+++ CyberScroller/src/net/griscraft/cyberscroller/objects/Player.java
@@ -5,7 +5,9 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 
 public class Player extends GameObject {
-
+	
+	private boolean left = false, right = false;
+	
 	public Player(int x, int y, ObjectHandler handler) {
 		super(x, y, handler);
 	}
@@ -13,15 +15,43 @@ public class Player extends GameObject {
 	public void tick() {
 		x += velX;
 		y += velY;
+		
+		//Movement
+		if (right) velX = 5;
+		else if (!left) velX = 0;
+		
+		if (left) velX = -5;
+		else if (!right) velX = 0;
+		
 	}
 
 	public void render(Graphics g) {
 		g.setColor(Color.BLUE);
 		g.fillRect(x, y, 32, 64);
 	}
+	
+	public void jump() {
+		
+	}
 
 	public Rectangle getBounds() {
 		return new Rectangle(x, y, 32, 64);
+	}
+
+	public boolean isLeft() {
+		return left;
+	}
+
+	public void setLeft(boolean left) {
+		this.left = left;
+	}
+
+	public boolean isRight() {
+		return right;
+	}
+
+	public void setRight(boolean right) {
+		this.right = right;
 	}
 
 }
