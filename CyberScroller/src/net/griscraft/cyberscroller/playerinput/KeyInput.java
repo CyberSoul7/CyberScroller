357c7468877804f51eb9388d281b5d4e1b2c5400
William Keller <komputerbuilder@gmail.com>
2017-11-19 07:28:23 -0600

    Completed KeyInput Class

--- CyberScroller/src/net/griscraft/cyberscroller/playerinput/KeyInput.java
+++ CyberScroller/src/net/griscraft/cyberscroller/playerinput/KeyInput.java
@@ -4,6 +4,7 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 import net.griscraft.cyberscroller.Game;
+import net.griscraft.cyberscroller.objects.Player;
 
 public class KeyInput extends KeyAdapter {
 	
@@ -16,11 +17,35 @@ public class KeyInput extends KeyAdapter {
 	public void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
 		
+		Player player = game.getPlayer();
+		
+		switch (key) {
+		case KeyEvent.VK_W:
+			player.jump();
+			break;
+		case KeyEvent.VK_A:
+			player.setLeft(true);
+			break;
+		case KeyEvent.VK_D:
+			player.setRight(true);
+			break;
+		}
 		
 	}
 	
 	public void keyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
 		
+		Player player = game.getPlayer();
+		
+		switch (key) {
+		case KeyEvent.VK_A:
+			player.setLeft(false);
+			break;
+		case KeyEvent.VK_D:
+			player.setRight(false);
+			break;
+		}
 	}
 	
 }
